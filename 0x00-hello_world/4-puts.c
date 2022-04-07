07c1affec5147a05dc1fa9305d053894b33e344a
sylviajebet <sylviajebett@gmail.com>
2022-04-07 10:35:06 -0700

    Betty rules implemented

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
 
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
