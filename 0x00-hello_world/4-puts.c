36b7287a22cd5203f1433084c12fc30bfbe18727
sylviajebet <sylviajebett@gmail.com>
2022-04-07 10:18:57 -0700

    A c file using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/* 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
