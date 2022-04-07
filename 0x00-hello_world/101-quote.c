58d39746cae7727479f21d39978a1339d6c795d7
sylviajebet <sylviajebett@gmail.com>
2022-04-07 12:34:13 -0700

    C file corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
   * main - First function to run
   *
-  * Return: 1 for success
+  * Return: 1 for error
   */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
