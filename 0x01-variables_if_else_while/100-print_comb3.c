ce2d73084bee65a35e4fc8acc4e330c96ffd1abd
sylviajebet <sylviajebett@gmail.com>
2022-04-08 14:22:31 -0700

    File corrected

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,7 +17,9 @@ int main(void)
 			{
 				putchar(num1);
 				putchar(num2);
-				if (num2 != 56 || num2 != 57)
+				if (num1 == 56 || num2 == 57)
+					break;
+				else
 				{
 					putchar(',');
 					putchar(' ');
