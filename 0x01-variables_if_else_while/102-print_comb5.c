3f44cea0d2b45e59242c419647fce383af936732
sylviajebet <sylviajebett@gmail.com>
2022-04-08 15:12:25 -0700

    C file to print 2 two digit combinations once

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point for a program to print two two-digit combos
+  *
+  * Return: 0 for succes
+  */
+int main (void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = 0; num2 <= 99; num2++)
+		{
+			if (num2 > num1)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
