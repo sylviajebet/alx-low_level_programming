45cdda5a131bf97d26bead9346dc0a0193e18433
sylviajebet <sylviajebett@gmail.com>
2022-04-12 15:05:55 -0700

    main.h edited, print alphabet program added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h";
+
+/**
+  * main - Entry point for a program to print lowercase alphabet
+  *
+  * Return: void for success
+  */
+void print_alphabet(void)
+{
+	char ltr;
+
+	for (ltr = 'a'; ltr <= 'z'; ltr++)
+		_putchar(ltr);
+	_putchar('\n');
+}
