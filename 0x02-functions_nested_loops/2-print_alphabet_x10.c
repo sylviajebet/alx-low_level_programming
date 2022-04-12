c9298fa338313df9ab59bbca3bab843b8b065686
sylviajebet <sylviajebett@gmail.com>
2022-04-12 15:13:42 -0700

    main.h edited, filr to print lowercase alphabet 10 times added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - Program to print lowercase alphabet 10 times
+  *
+  * Return: void for success
+  */
+void print_alphabet_x10(void)
+{
+	int count;
+	char ltr;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (ltr = 'a'; ltr <= 'z'; ltr++)
+			_putchar(ltr);
+	}
+	_putchar('\n');
+}
