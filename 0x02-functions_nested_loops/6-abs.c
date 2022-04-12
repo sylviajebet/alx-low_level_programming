b892c6b8e4b62fd8b60dda923f54a45affa34ffe
sylviajebet <sylviajebett@gmail.com>
2022-04-12 15:48:44 -0700

    main.h edited, file to give the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _abs - Program to get absolute value of an integer
+  * @n: n is an integer
+  *
+  * Return: integer
+  */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
