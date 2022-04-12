dbe98595f3c555f3a48c7b73fa8426491e2b9744
sylviajebet <sylviajebett@gmail.com>
2022-04-12 16:48:12 -0700

    main file edited, file to add 2 integers and return sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add - Program to add two integers
+  * @n1: n1 is the first integer
+  * @n2: n2 is the second integer
+  *
+  * Return: integer of the sum
+  */
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+
+	return (sum);
+}
