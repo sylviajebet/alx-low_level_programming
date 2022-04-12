764af742dbae19048414b54fcfaae7cd7f5c6b18
sylviajebet <sylviajebett@gmail.com>
2022-04-12 16:21:39 -0700

    File corrected

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,15 @@ void jack_bauer(void)
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max = 58;
 
-	for (hours_tens = 0; hours_tens < 3; hours_tens++)
+	for (hours_tens = '0'; hours_tens < '3'; hours_tens++)
 	{
-		if (hours_tens == 2)
-			hours_max = 4;
-		for (hours_ones = 0; hours_ones < hours_max; hours_ones++)
+		if (hours_tens == '2')
+			hours_max = '4';
+		for (hours_ones = '0'; hours_ones < hours_max; hours_ones++)
 		{
-			for (minutes_tens = 0; minutes_tens < 6; minutes_tens++)
+			for (minutes_tens = '0'; minutes_tens < '6'; minutes_tens++)
 			{
-				for (minutes_ones = 0; minutes_ones < 58; minutes_ones++)
+				for (minutes_ones = '0'; minutes_ones < 58; minutes_ones++)
 				{
 					_putchar(hours_tens);
 					_putchar(hours_ones);
