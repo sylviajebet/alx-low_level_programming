b6e86659f78a6a448f0d74110beafeb84449de9d
sylviajebet <sylviajebett@gmail.com>
2022-04-12 17:03:03 -0700

    File corrected

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,21 @@
   */
 void print_to_98(int n)
 {
-	while (n < 98)
+	if (n < 98)
 	{
-		printf("%i", n);
-		n++;
+		while (n < 98)
+		{
+			printf("%i", n);
+			n++;
+		}
 	}
-
-	while (n > 98)
+	else if (n > 98)
 	{
-		printf("%i", n);
-		n++;
+		while (n > 98)
+		{
+			printf("%i", n);
+			n++;
+		}
 	}
 
 	printf("98\n");
