f0d6765822ad020d30979b98e460ebc715749aa3
sylviajebet <sylviajebett@gmail.com>
2022-04-12 17:55:07 -0700

    File corrected

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,15 +9,15 @@
 void print_times_table(int n)
 {
 	int row, column, product, hundreds, tens, ones;
+	
+	if (n < 0 || n > 15)
+		return;
 
 	for (row = 0; row <= n; row++)
 	{
 		for (column = 0; column <= n; column++)
 		{
 			product = row * column;
-			hundreds = product / 100;
-			tens = product / 10;
-			ones = product % 10;
 
 			if (column == 0)
 				_putchar('0');
@@ -27,23 +27,23 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(ones + '0');
+				_putchar(product % 10 + '0');
 			}
 			else if (product >= 10 && product < 100)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(tens % 10 + '0');
-				_putchar(ones + '0');
+				_putchar((product / 10) % 10 + '0');
+				_putchar(product % 10 + '0');
 			}
 			else if (product > 99 && product < 1000)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(hundreds % 10 + '0');
-				_putchar(tens + '0');
-				_putchar(ones + '0');
+				_putchar(product / 100 + '0');
+				_putchar((product / 10) % 10 + '0');
+				_putchar(product % 10 + '0');
 			}
 		}
 		_putchar('\n');
