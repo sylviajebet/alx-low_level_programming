50609205eff56790074eb25e1069bcddbc6048ef
sylviajebet <sylviajebett@gmail.com>
2022-04-14 17:31:30 +0300

    File corrected

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0)
-			printf("%s", "Fizz");
+		if ((num % 3 == 0) && (num % 5 == 0))
+			printf("%s", "FizzBuzz");
 		else if (num % 5 == 0)
 			printf("%s", "Buzz");
-		else if ((num % 3 == 0) && (num % 5 == 0))
-			printf("%s", "FizzBuzz");
+		else if (num % 3 == 0)
+			printf("%s", "Fizz");
 		else
 			printf(num);
 
