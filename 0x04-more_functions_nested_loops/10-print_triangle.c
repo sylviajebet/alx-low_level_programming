a3defe110e8d6e35ca88875971438980220748aa
sylviajebet <sylviajebett@gmail.com>
2022-04-14 19:04:02 +0300

    Program to print triangle of size size

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - Program to print a triangle of size size
+ * @size: size is an integer
+ *
+ * Return: 0 for success
+ */
+
+void print_triangle(int size)
+{
+	int i, j, space;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (j = 1; j <= size; j++)
+		{
+			for (space = 0; space <= (size - 1); space++)
+				_putchar(' ');
+			for (i = 1; i <= j; i++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
