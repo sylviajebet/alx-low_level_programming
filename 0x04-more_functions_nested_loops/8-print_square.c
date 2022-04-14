2f0503553d1a3c7ed25e7ab3a72ac066b9a9e432
sylviajebet <sylviajebett@gmail.com>
2022-04-14 16:51:23 +0300

    Program to print a square of size size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - Program to print a square of size size
+ * @size: size is an integer
+ *
+ * Return: 0 for success
+ */
+
+void print_square(int size)
+{
+	int i;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		while (size > 0)
+		{
+			for (i = 0; i < size; i++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
