caa3bae3b7a2ef785972b78340866723910a83dc
sylviajebet <sylviajebett@gmail.com>
2022-04-20 22:49:57 +0300

    Program to printa string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Program to print a string in reverse
+ * @s: Pointer
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+    int i, j;
+    char str[strlen(s)];
+
+    for (i = 0; i < strlen(s); i++)
+    {
+        for (j = strlen(s); j > 0; j--)
+        {
+            str[i] = s[j];
+            break;
+        }
+    }
+}
