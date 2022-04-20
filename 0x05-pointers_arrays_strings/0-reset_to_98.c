065c9a4782db62d13e76b773b8fb4475f5627c11
sylviajebet <sylviajebett@gmail.com>
2022-04-20 20:25:38 +0300

    File updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,12 +4,10 @@
 /**
  * reset_to_98 - Program to take a pointer and change value to 98
  * @n: Pointer
- * Return: 0 for success
+ * Return: void
  */
 
 void reset_to_98(int *n)
 {
-	int num = 98;
-
-	n = &num;
+	*n = 98;
 }
