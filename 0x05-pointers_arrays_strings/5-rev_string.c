731e62a8c3e85af9875b9593efbcf50dbd9fff0b
sylviajebet <sylviajebett@gmail.com>
2022-04-20 23:27:17 +0300

    Program to reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - Program that reverses a string
+ * @s: Pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, j, count = 0, temp;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	j = count - 1;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		temp = s[count];
+		s[count] = s[j];
+		s[j] = temp;
+		j--;
+	}
+}
