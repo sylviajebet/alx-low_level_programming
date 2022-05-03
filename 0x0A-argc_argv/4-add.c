fcd7a27970034e174254305ef4b8110ba1ea5171
sylviajebet <sylviajebett@gmail.com>
2022-05-04 02:39:22 +0300

    Files corrected

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
   * main - Program that adds positive numbers
@@ -10,27 +11,25 @@
 
 int main(int argc, char *argv[])
 {
-	int idx, idx2 = argc - 1, sum = 0;
+	int idx, idx2, sum = 0;
 
 	if (argc < 2)
 	{
 		printf("0\n");
 		return (0);
 	}
-
-	for (; idx2; idx2--)
+	for (idx2 = 1; idx2 < argc; idx2++)
 	{
-		idx = atoi(argv[idx2]);
-
-		if (idx == 0)
+		for (idx = 0; argv[idx2][idx]; idx++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!(isdigit(argv[idx2][idx])))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += idx;
+		sum += atoi(argv[idx2]);
 	}
-
 	printf("%d\n", sum);
-
 	return (0);
 }
