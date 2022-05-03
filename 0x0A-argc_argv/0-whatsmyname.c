b1791e542dadc42abed0b21b5e9954078d5e1656
sylviajebet <sylviajebett@gmail.com>
2022-05-04 00:53:37 +0300

    Function to print a file's name and new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Function that writes its own name
+ * @argc: Argument count
+ * @argv: Array of pointers to argument
+ * Return: 0 for success
+ **/
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
