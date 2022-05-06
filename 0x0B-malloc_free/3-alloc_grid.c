844f91ac82f1f5a541370a9d3313df2a2fa2981e
sylviajebet <sylviajebett@gmail.com>
2022-05-06 19:58:02 +0300

    File corrected

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,28 +13,30 @@ int **alloc_grid(int width, int height)
 {
 	int **arr, idx, idx2;
 
-	if (width < 0 || height < 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-
-	arr = (int **) malloc(sizeof(int *) * height);
-	if (arr == NULL)
-		return (NULL);
-	for (idx = 0; idx < height; idx++)
+	else
 	{
-		arr[idx] = (int *) malloc(width * sizeof(int));
-		if (!arr[idx])
-		{
-			for (idx2 = 0; idx2 <= idx; idx2++)
-				free(arr[idx2]);
-			free(arr);
+		arr = (int **) malloc(sizeof(int *) * height);
+		if (arr == NULL)
 			return (NULL);
-		}
+		for (idx = 0; idx < height; idx++)
+		{
+			arr[idx] = (int *) malloc(width * sizeof(int));
+			if (!arr[idx])
+			{
+				for (idx2 = 0; idx2 <= idx; idx2++)
+					free(arr[idx2]);
+				free(arr);
+				return (NULL);
+			}
 
+		}
+		for (idx = 0; idx < height; idx++)
+		{
+			for (idx2 = 0; idx2 < width; idx2++)
+				arr[idx][idx2] = 0;
+		}
+		return (arr);
 	}
-	for (idx = 0; idx < height; idx++)
-	{
-		for (idx2 = 0; idx2 < width; idx2++)
-			arr[idx][idx2] = 0;
-	}
-	return (arr);
 }
