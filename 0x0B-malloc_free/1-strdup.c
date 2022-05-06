565299554c469fed639ca92049b8b42677412449
sylviajebet <sylviajebett@gmail.com>
2022-05-06 18:48:34 +0300

    File corrected

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * strdup - Function that returns a pointer to
+ * _strdup - Function that returns a pointer to
  * a newly allocated space in memory which contains
  * a copy of the string given as a parameter
  *
