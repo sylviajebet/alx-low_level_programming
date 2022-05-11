889141ecd3d89af5c209a037104bf427416f9387
sylviajebet <sylviajebett@gmail.com>
2022-05-12 00:42:36 +0300

    Two functions added to mull: memset and calloc, main.h updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * _memset - Function that fills memory with a constant byte
+  * @s: Pointer to memory
+  * @b: Constant byte
+  * @n: Integer, number of bytes
+  * Return: Pointer to memory area
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * _calloc - Function that allocates memory for an array, using _memset
+ *
+ * @nmemb: Int for number of elements in the array
+ * @size: Int for size of each element in the array
+ * Return: Pointer value to the allocated memory, otherwise NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	_memset(arr, 0, nmemb * size);
+
+	return (arr);
+}
 
 /**
  * mul_numbers - Program that multiplies two positive numbers
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
