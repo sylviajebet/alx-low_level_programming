15382d6038e2499d8fbcdaa4fc9dd6b187d8d44a
sylviajebet <sylviajebett@gmail.com>
2022-05-16 23:57:30 +0300

    File op_functions.c to perform the integer functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "calc.h"
+
+/**
+ * op_add - Function to return sum of two numbers
+ *
+ * @a: First ineteger
+ * @b: Second integer
+ * Return: Integer sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Function to return difference of two numbers
+ *
+ * @a: First ineteger
+ * @b: Second integer
+ * Return: Integer difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Function to return product of two numbers
+ *
+ * @a: First ineteger
+ * @b: Second integer
+ * Return: Integer product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function to quotient sum of two numbers
+ *
+ * @a: First ineteger
+ * @b: Second integer
+ * Return: Integer quotient
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function to return modulus of two numbers
+ *
+ * @a: First ineteger
+ * @b: Second integer
+ * Return: Integer modulus
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
