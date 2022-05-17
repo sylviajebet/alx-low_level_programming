7b7bbaa4bde7f625286be6370ac770fc368401f6
sylviajebet <sylviajebett@gmail.com>
2022-05-17 14:07:06 +0300

    Function_pointers.h file updated

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
