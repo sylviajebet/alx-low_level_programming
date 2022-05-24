dac6cb52e3ebc95ec1e26133fcf4b80bfc4d5169
sylviajebet <sylviajebett@gmail.com>
2022-05-25 02:01:57 +0300

    File corrected

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * str_len - Function to find the length of a string
+ * _strlen - Function to find the length of a string
  * @str: The string to find length of
  * Return: Integer length of a string
  */
 int _strlen(const char *str)
 {
-	int count;
+	int count = 0;
 
 	while (str[count])
 		count++;
@@ -23,21 +23,10 @@ int _strlen(const char *str)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *val;
-	char *dupstr;
-	int idx;
 
-	if (str == NULL || head == NULL)
-		return (NULL);
-	val = *head;
-	dupstr = malloc((_strlen(str) + 1) * sizeof(char));
-	if (dupstr == NULL)
-		return (NULL);
-	for (idx = 0; str[idx]; idx++)
-		dupstr[idx] = str[idx];
 	val = malloc(sizeof(list_t));
 	if (val == NULL)
 	{
-		free(dupstr);
 		return (NULL);
 	}
 	val->next = *head;
