ce82d989a35309eab213b6c47f87f6bf9b9a55bb
sylviajebet <sylviajebett@gmail.com>
2022-06-01 22:03:48 +0300

    File corrected

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,17 +11,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int val = 0;
 	int strlen = 0;
 
-	if (b[strlen] != '\0')
-	{
-		while ((b[strlen] == '0') || (b[strlen] == '1'))
-		{
-			val <<= 1;
-			val += b[strlen] - '0';
-			strlen++;
-		}
-
-		return (val);
-	}
-	else
+	if (!b[strlen])
 		return (0);
+
+	while ((b[strlen] == '0') || (b[strlen] == '1'))
+	{
+		val <<= 1;
+		val += b[strlen] - '0';
+		strlen++;
+	}
+
+	return (val);
 }
