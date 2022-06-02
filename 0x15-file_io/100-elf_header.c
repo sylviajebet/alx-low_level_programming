b781f94f98e2bccfedba382aca05d56f1e2d7548
sylviajebet <sylviajebett@gmail.com>
2022-06-02 22:16:00 +0300

    File corrected

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -20,10 +20,10 @@ void check_elf(unsigned char *e_ident)
 	}
 }
 /**
- * print_magic - Prints magic numbers of an ELF header
+ * print_data - Prints magic numbers of an ELF header
  * @e_ident: Pointer to an array containing ELF magic numbers
  */
-void print_magic(unsigned char *e_ident)
+void print_data(unsigned char *e_ident)
 {
 	int index;
 
@@ -37,13 +37,7 @@ void print_magic(unsigned char *e_ident)
 		else
 			printf(" ");
 	}
-}
-/**
- * print_class - Prints the class of an ELF header
- * @e_ident: Pointer to an array containing the ELF class
- */
-void print_class(unsigned char *e_ident)
-{
+
 	printf("  Class:                             ");
 
 	switch (e_ident[EI_CLASS])
@@ -60,13 +54,7 @@ void print_class(unsigned char *e_ident)
 	default:
 		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
-}
-/**
- * print_data - Prints the data of an ELF header
- * @e_ident: Pointer to an array containing the ELF class
- */
-void print_data(unsigned char *e_ident)
-{
+
 	printf("  Data:                              ");
 
 	switch (e_ident[EI_DATA])
@@ -83,13 +71,7 @@ void print_data(unsigned char *e_ident)
 	default:
 		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
-}
-/**
- * print_version - Prints the version of an ELF header
- * @e_ident: Pointer to an array containing the ELF version
- */
-void print_version(unsigned char *e_ident)
-{
+
 	printf("  Version:                           %d",
 	       e_ident[EI_VERSION]);
 
@@ -102,13 +84,7 @@ void print_version(unsigned char *e_ident)
 		printf("\n");
 		break;
 	}
-}
-/**
- * print_osabi - Prints the OS/ABI of an ELF header
- * @e_ident: Pointer to an array containing the ELF version
- */
-void print_osabi(unsigned char *e_ident)
-{
+
 	printf("  OS/ABI:                            ");
 
 	switch (e_ident[EI_OSABI])
@@ -146,13 +122,7 @@ void print_osabi(unsigned char *e_ident)
 	default:
 		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 	}
-}
-/**
- * print_abi - Prints the ABI version of an ELF header
- * @e_ident: Pointer to an array containing the ELF ABI version
- */
-void print_abi(unsigned char *e_ident)
-{
+
 	printf("  ABI Version:                       %d\n",
 	       e_ident[EI_ABIVERSION]);
 }
@@ -259,12 +229,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
-	print_magic(header->e_ident);
-	print_class(header->e_ident);
 	print_data(header->e_ident);
-	print_version(header->e_ident);
-	print_osabi(header->e_ident);
-	print_abi(header->e_ident);
 	print_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
 
