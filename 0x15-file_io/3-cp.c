4ad705c89356e7847a2b5b40b3dfc555c020edab
sylviajebet <sylviajebett@gmail.com>
2022-06-02 15:41:08 +0300

    File corrected

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,50 +1,75 @@
 #include "main.h"
+/**
+ * error_type - Function to state error type
+ * @err_num: Error number
+ * @filename: Name of file
+ */
+void error_type(int err_num, char *filename)
+{
+	if (err_num == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(err_num);
+	}
+	if (err_num == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(err_num);
+	}
+}
+/**
+* cp_file - Function to copy a file to another
+ * @file_from: File to copy content from
+ * @file_to: File to copy content to
+ * Return: Void
+ */
+void cp_file(char *file_from, char *file_to)
+{
+	int from_read, from_write, to_read, to_write;
+	char buf[1024];
 
+	from_read = open(file_from, O_RDONLY);
+	if (from_read == -1)
+		error_type(98, file_from);
+	from_write = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, 0064);
+	if (from_write == -1)
+	{
+		close(from_read);
+		error_type(99, file_to);
+	}
+	do {
+		to_read = read(from_read, buf, 1024);
+		if (to_read < 0)
+			error_type(98, file_from);
+		to_write = write(from_write, buf, to_read);
+		if (to_write < to_read)
+			error_type(99, file_to);
+	}	while (to_write == 1024);
+	if (close(from_read) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file %i\n", from_read);
+		close(from_write);
+		exit(100);
+	}
+	if (close(from_write) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file %i\n", from_write);
+		exit(100);
+	}
+}
 /**
  * main - Program that copies the content of a file to another file
  * @argc: Count of the argument
  * @argv: Vector of the argument
  * Return: 0 for success
  */
-
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, file_size;
-	char buf[1024];
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY, 0);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0064);
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while ((file_size = read(file_from, buf, 1024)) > 0)
-	{
-		if (write(file_to, buf, file_size) != file_size)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-	if (file_size == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (close(file_from) != 0)
-		dprintf(STDERR_FILENO, "Error: Can't close file %i\n", file_from), exit(100);
-	if (close(file_to) != 0)
-		dprintf(STDERR_FILENO, "Error: Can't close file %i\n", file_to), exit(100);
+	cp_file(argv[1], argv[2]);
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_type(int err_num, char *filename);
+void cp_file(char *file_from, char *file_to);
 int main(int argc, char *argv[]);
 
 #endif
